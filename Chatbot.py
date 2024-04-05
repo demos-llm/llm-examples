9f498f789515f321d7e4bbdb1c28a08e7a476ab7
Marcin Bodych <bodychmarcin@gmail.com>
2024-04-05 22:11:14 +0000

    up

--- Chatbot.py
+++ Chatbot.py
@@ -14,7 +14,6 @@ with st.sidebar:
 
 st.title("💬 Anschreiben Assistant")
 st.caption("🖋️ Der Anschreiben Assistant generiert ein ideales Anschreiben für dich")
-message = st.chat_message("assistant")
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "Hallo, möchten Sie, dass ich Ihnen helfe, das perfekte Anschreiben für Sie zu verfassen?"}]
 
@@ -22,23 +21,57 @@ avatars = {'assistant': '🧙‍♀️', 'user': '👤'}
 for msg in st.session_state.messages:
     st.chat_message(msg["role"], avatar=avatars[msg["role"]]).write(msg["content"])
 
+uploaded_file = st.file_uploader("Wähle eine Datei")
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    if "uploaded_file" not in st.session_state or not st.session_state["uploaded_file"]:
+        st.session_state["uploaded_file"] = bytes_data
+
 if prompt := st.chat_input(placeholder='Ihre Nachricht'):
     if not openai_api_key:
         st.info("Bitte fügen Sie Ihren OpenAI-API-Schlüssel hinzu, um fortzufahren.")
         st.stop()
 
     client = OpenAI(api_key=openai_api_key)
-    thread = client.beta.threads.create()
-    message = client.beta.threads.messages.create(
-        thread_id=thread.id,
-        role="user",
-        content=prompt
-    )
+    thread_id = None
+    if "thread" not in st.session_state:
+        thread = client.beta.threads.create()
+        thread_id = thread.id
+        st.session_state["thread"] = thread_id
+    else:
+        thread_id = st.session_state["thread"]
+    for msg in st.session_state.messages:
+        client.beta.threads.messages.create(
+            thread_id=thread_id,
+            role=msg["role"],
+            content=msg["content"]
+        )
+    if "uploaded_file" in st.session_state and st.session_state["uploaded_file"]:
+        bytes_data = st.session_state["uploaded_file"]
+        file_response = client.files.create(
+            file=bytes_data,
+            purpose="assistants"
+        )
+
+        message = client.beta.threads.messages.create(
+            thread_id=thread_id,
+            role="user",
+            content=prompt,
+            file_ids = [file_response.id]
+        )
+        
+        st.session_state["uploaded_file"] = None
+    else:
+        message = client.beta.threads.messages.create(
+            thread_id=thread_id,
+            role="user",
+            content=prompt
+        )
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user", avatar=avatars['user']).write(prompt)
     with st.chat_message("assistant", avatar=avatars['assistant']):
         with client.beta.threads.runs.stream(
-        thread_id=thread.id,
+        thread_id=thread_id,
         assistant_id='asst_WxG5MfdQBpkaZ7kzT5iuYoWp'
         ) as stream:
             msg = st.write_stream(process_stream(stream))
